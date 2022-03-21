373b27732821b2a8daf3562a0d8bf34f185be8f4
Pablo Dapena Fernández <98289035+pdapena97@users.noreply.github.com>
2022-03-21 19:40:54 +0100

    Add files via upload

--- js/fetch.js
+++ js/fetch.js
@@ -1,22 +1,24 @@
 "use strict"
+
+const datos = [];
+
+
 // query selector para generar contenedores dinamicamente
 const eventList = document.querySelector("#lineatemporal");
+const form = document.querySelector("form")
 
-// Almaceno los datos del USER
-const dataUser = Array.from(document.querySelectorAll('#formulario input')).reduce((acc, input) => ({...acc,[input.id]: input.value}),{});
 
 // funcion para generar eventos y añadirlos dinamicamente al html
 const generateEvents = (data) => {
     //ordeno los eventos cronológicamente
-    data.eventos.push(dataUser);
+   // data.eventos.push(dataUser);
     data.eventos.sort((a, b) => a.year - b.year);
     
 
     data.eventos.forEach((suceso) => {
-      // creo un section en el que guardo cada suceso
+      // creo una lista en el que guardo cada suceso
       const newEventList = document.createElement('li');
-      // añado  clases
-      
+      // añado clases para CSS
       newEventList.classList.add("orderedlist");
       // creo tags HTML para cada suceso
       const eventoHTML = `
@@ -34,63 +36,42 @@ const generateEvents = (data) => {
 };
 
 
-//fetch de nuestros datos
-//Creo que no tiene sentido usar try y catch error en caso de que ocurra
-//un error en la petición, ya que estoy pidiendo archivos en local
-//vamos, que mi modulo JSON está en local.
-//no uso try y catch para optimizar código.
-//si estuviera pidiendo datos a una api sí que usaría await.. try.. catch
-fetch("data/data.json")
-.then(response => response.json())
-.then(data => generateEvents(data))
+
+// fetch de nuestros datos
+async function  main(){
+  const resp = await fetch("data/data.json")
+  const datos =  await resp.json()
+  
+  generateEvents(datos);
+
+}
+
+main()
+
+
+// TENGO QUE AÑADIR TRY CATCH
 
 
 
-
-
-/*
-//ALMACENO LOS DATOS DEL USER
 //const dataUser = Array.from(document.querySelectorAll('#formulario input')).reduce((acc, input) => ({...acc,[input.id]: input.value}),{});
 
 const manejadoraFormSubmit = (event) => {
   event.preventDefault();
-
-  const generateEvents = (data) => {
-    //ordeno los eventos cronológicamente
-    data.eventos.push(dataUser);
-    data.eventos.sort((a, b) => a.year - b.year);
-    
-
-    data.eventos.forEach((suceso) => {
-      // creo un section en el que guardo cada suceso
-      const newEventList = document.createElement('li');
-      // añado  clases
-      // en teoría esto no es necesario...?
-      // o sí, tengo que mirar, porque va añadiendo linea por linea
-      newEventList.classList.add("orderedlist");
-      // creo tags HTML para cada suceso
-      //aquí puedo hacer poner exactamente como quiera mi estructura html
-      //muy util para mirar CSS por ahi que suelen ir con divs
-      const eventoHTML = `
-          <div>
-              <time> ${suceso.year} : ${suceso.title} </time>
-              <img src=${suceso.image} alt="image" height="250" width="250" />
-              <p>${suceso.text}<p> 
-             </div>
-        </li>
-      `;
-      //los añado al HTML
-      newEventList.innerHTML = eventoHTML;
-      eventList.appendChild(newEventList);
-  });
-};
-
+  // leer los datos del formulario
+  const dataUser = {
+    year: document.getElementById("year").value,
+    title: document.getElementById("title").value,
+    image: document.getElementById("image").value,
+    text: document.getElementById("text").value
+  }
+  // crear el objeto
+  
+  // añadirlo a array datos
+  datos.eventos.push(dataUser);
+  
+  // llamar 
+  generateEvents(datos);
+  
 }
 
-
-fetch("data/data.json")
-.then(response => response.json())
-.then(data => generateEvents(data))
-
 form.addEventListener("submit", manejadoraFormSubmit);
-*/
